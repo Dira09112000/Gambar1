859ad6ddacbcad4cdc7f76e545be7482bcd2119a
Dira09112000 <dira_aris_saputra@teknokrat.ac.id>
2022-08-23 08:05:13 +0700

    jadi

--- src/components/Grafik.js
+++ src/components/Grafik.js
@@ -10,6 +10,7 @@ import {
   Title,
   Tooltip,
   Legend,
+  Filler,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 const Grafik = () => {
@@ -20,33 +21,61 @@ const Grafik = () => {
     LineElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    Filler,
   );
 
   const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: false,
-        text: "",
-      },
-    },
+    scales: {
+      xAxes: [{
+          gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }
+      }],
+      yAxes: [{
+          gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }   
+      }]
+  }
   };
 
-  const labels = ["1PM", "4PM", "7PM", "50PM", "1AM", "4AM", "7AM", "10AM"];
-
   const data = {
-    labels,
+    labels: ["10AM", "1PM", "4PM", "7PM", "10PM", "1AM", "4AM", "7AM", "10AM"],
     datasets: [
       {
-        label: "",
-        data: ["48.000", "47.500", "47.000", "46.500", "46.000"],
-        borderColor: "#6464D9",
         fill: true,
-        backgroundColor: "yellow",
+        tension: 0.1,
+        pointRadius: 0,
+        pointBackgroundColor: "purple",
+        fillColor: "rgba(250,174,50,0.5)",
+        strokeColor: "#ff6c23",
+        pointColor: "#fff",
+        pointStrokeColor: "#ff6c23",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "#ff6c23",
+        backgroundColor: "purple",
+        borderWidth: 10,
+        borderColor: "blue",
+        fill: {
+          target: "origin",
+          above: "rgb(227, 229, 249)", // Area will be red above the origin
+        },
+
+        data: [
+          48879, 48773, 46538, 48762, 46903, 47595, 46818, 47561, 47636, 48307,
+          46257, 47676, 46980, 46340, 48994, 47960, 48975, 46139, 47085, 47852,
+          46656, 46248, 47050, 47354, 46331, 47364, 46375, 46968, 47116, 47145,
+          46598, 46365, 46544, 46265, 47149, 46887, 46816, 47331, 46584, 47715,
+          46474, 46437, 46354, 46409, 46792, 46552, 47755, 46504, 46670, 47210,
+          47293, 46305, 47436, 46556, 47107, 47123, 47637, 47213, 47851, 46307,
+          46384, 47489, 46452, 46601, 47238, 47624, 46150, 47934, 46532, 46818,
+          46388, 46472, 47241, 46162, 47833, 46341, 47313, 46515, 46612, 47151,
+          47929, 46890, 47168, 46999, 46456, 47737, 47235, 46498, 46511, 46311,
+          47203, 47529, 47775, 47128, 46532, 46712, 46195, 46820, 46710, 46292,
+          46788, 46025, 47969, 46071, 46657, 47782, 46414, 46148, 46436, 47793,
+          47780, 46149, 47078, 46815, 46308, 46803, 46514, 46084, 46637,
+        ],
       },
     ],
   };
--- src/components/Grafik2.js
+++ src/components/Grafik2.js
@@ -1,4 +1,6 @@
 import "../css/grafik2.css";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
 
 const Grafik2 = () => {
   return (
@@ -8,21 +10,141 @@ const Grafik2 = () => {
           style={{
             width: "100%",
             height: "auto",
-            background: "black",
             display: "flex",
           }}
         >
           <div>
             <div style={{ display: "flex" }}>
-              <div className="kotak">KOTAK 1</div>
-              <div className="kotak">KOTAK 2</div>
+              <div className="card-bnb" style={{ paddingLeft: "5px" }}>
+                <div style={{ display: "flex", paddingBottom: "80px" }}>
+                  <img
+                    className="img-bnb"
+                    src="https://w7.pngwing.com/pngs/1008/997/png-transparent-bnb-binance-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon.png"
+                  ></img>
+                  <p className="text-bnb-usd">BNB-USD</p>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <button className="button-card">+1.153.08%</button>
+                  {""}
+                </div>
+                <div className="text-dolar">
+                  <text>$47,061.18</text>
+                </div>
+              </div>
+
+              <div className="card-btc" style={{ paddingLeft: "5px" }}>
+                <div style={{ display: "flex", paddingBottom: "80px" }}>
+                  <img
+                    className="img-bnb"
+                    src="https://w7.pngwing.com/pngs/1008/997/png-transparent-bnb-binance-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon.png"
+                  ></img>
+                  <p className="text-bnb-usd">ETH-USM</p>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <button className="button-card-btc">+1.153.08%</button>
+                  {""}
+                </div>
+                <div className="text-dolar">
+                  <text>$47,061.18</text>
+                </div>
+              </div>
             </div>
+
             <div style={{ display: "flex" }}>
-              <div className="kotak">KOTAK 3</div>
-              <div className="kotak">KOTAK 4</div>
+              <div className="card-bnb" style={{ paddingLeft: "5px" }}>
+                <div style={{ display: "flex", paddingBottom: "80px" }}>
+                  <img
+                    className="img-bnb"
+                    src="https://w7.pngwing.com/pngs/1008/997/png-transparent-bnb-binance-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon.png"
+                  ></img>
+                  <p className="text-bnb-usd">BNB-USD</p>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <button className="button-card">+1.153.08%</button>
+                  {""}
+                </div>
+                <div className="text-dolar">
+                  <text>$47,061.18</text>
+                </div>
+              </div>
+
+              <div className="card-bnb" style={{ paddingLeft: "5px" }}>
+                <div style={{ display: "flex", paddingBottom: "80px" }}>
+                  <img
+                    className="img-bnb"
+                    src="https://w7.pngwing.com/pngs/1008/997/png-transparent-bnb-binance-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon.png"
+                  ></img>
+                  <p className="text-bnb-usd">BNB-USD</p>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <button className="button-card">+1.153.08%</button>
+                  {""}
+                </div>
+                <div className="text-dolar">
+                  <text>$47,061.18</text>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="kotak-panjang">KOTAK 5</div>
+
+          <div style={{ paddingLeft: "15px" }}>
+            <Card className="card-chat">
+              <Card.Body>
+                <p className="reactions">214,957 reactions</p>
+                <div style={{ display: "flex" }}>
+                  <img
+                    className="icon-chat"
+                    src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                  />
+                  <p className="thomas" style={{ paddingLeft: "15px" }}>
+                    Thomas
+                  </p>
+                </div>
+                <div className="buble-chat">
+                  <Card>
+                    <Card.Body>
+                      <p className="isi-chat">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry.
+                      </p>
+                    </Card.Body>
+                  </Card>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <img
+                    className="icon-chat"
+                    src="https://cdn-icons-png.flaticon.com/512/206/206861.png"
+                  />
+                  <p className="thomas" style={{ paddingLeft: "15px" }}>
+                    Master Cat
+                  </p>
+                </div>
+                <div className="buble-chat">
+                  <Card>
+                    <Card.Body>
+                      <p className="isi-chat">
+                        orem Ipsum has been the industry's standard dummy text
+                        ever since the 1500s.
+                      </p>
+                    </Card.Body>
+                  </Card>
+                </div>
+
+                <div style={{ display: "flex", paddingTop: "50px" }}>
+                  <Form.Control
+                    className="chat-share"
+                    type="email"
+                    placeholder="Share your reaction"
+                  />
+                  <div style={{ paddingLeft: "5px" }}>
+                    <Card className="icon-chat-bawah">
+                      <Card.Body>{/* <AiOutlineSend/> */}</Card.Body>
+                    </Card>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
         </div>
       </div>
     </>
